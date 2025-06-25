6d7cb85e1a5f051f15959ce4e9e61ef8021b269f
grievous110 <130253759+grievous110@users.noreply.github.com>
2025-06-26 00:59:55 +0200

    Code generation function

--- lib/engine/two_factor_token.dart
+++ lib/engine/two_factor_token.dart
@@ -1,3 +1,9 @@
+import 'dart:typed_data';
+import 'dart:math';
+import 'package:base32/base32.dart';
+import 'package:pointycastle/api.dart';
+import 'package:pointycastle/macs/hmac.dart';
+
 class TOTPSecret {
   String issuer;
   String accountName;
@@ -23,6 +29,27 @@ class TOTPSecret {
       algorithm: json['algorithm'] as String? ?? 'SHA-1',
       period: json['period'] as int? ?? 30,
       digits: json['digits'] as int? ?? 6);
+  }
+
+  String generateTOTPCode({DateTime? timestamp}) {
+    final DateTime now = timestamp ?? DateTime.now().toUtc();
+    final int secondsSinceEpoch = now.millisecondsSinceEpoch ~/ 1000;
+    final int counter = secondsSinceEpoch ~/ period;
+
+    final Uint8List decodedSecret = base32.decode(secret);
+    final ByteData timeBytes = ByteData(8)..setInt64(0, counter, Endian.big);
+
+    final HMac hmac = HMac(Digest(algorithm), 64)..init(KeyParameter(decodedSecret));
+    final Uint8List hash = hmac.process(timeBytes.buffer.asUint8List());
+
+    final int offset = hash.last & 0xf;
+    final int binary = ((hash[offset] & 0x7f) << 24) |
+    ((hash[offset + 1] & 0xff) << 16) |
+    ((hash[offset + 2] & 0xff) << 8) |
+    (hash[offset + 3] & 0xff);
+
+    final int otp = binary % pow(10, digits).toInt();
+    return otp.toString().padLeft(digits, '0');
   }
 
   Map<String, dynamic> toJson() {
