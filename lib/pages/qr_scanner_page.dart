b3a692da8cc090a14979225e899d55bbbcd4adae
grievous110 <130253759+grievous110@users.noreply.github.com>
2025-06-28 15:09:21 +0200

    QR-Scanner page

---
+++ lib/pages/qr_scanner_page.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:passwordmanager/pages/widgets/corner_border.dart';
+import 'package:passwordmanager/pages/widgets/default_page_body.dart';
+
+class QrScannerPage extends StatefulWidget {
+  const QrScannerPage({super.key, required this.onScan});
+
+  final Function(String) onScan;
+
+  @override
+  State<QrScannerPage> createState() => _QrScannerPageState();
+}
+
+class _QrScannerPageState extends State<QrScannerPage> {
+  bool _hasScanned = false;
+  bool _torchEnabled = false;
+  late MobileScannerController _controller;
+
+  void _handleScan(BarcodeCapture capture) {
+    if (_hasScanned) return;
+
+    final String? code = capture.barcodes.first.rawValue;
+    if (code != null && mounted) {
+      setState(() {
+        _hasScanned = true;
+      });
+      widget.onScan(code);
+      Navigator.pop(context);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = MobileScannerController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scan QR Code'),
+      ),
+      body: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25.0),
+              topRight: Radius.circular(25.0),
+            ),
+            child: MobileScanner(
+              controller: _controller,
+              overlayBuilder: (context, contraints) => Stack(children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: SizedBox(
+                        width: 150,
+                        child: Container(
+                          color: Colors.grey.withAlpha(100),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                icon: Icon(
+                                  _torchEnabled
+                                      ? Icons.flash_on_rounded
+                                      : Icons.flash_off_rounded,
+                                ),
+                                color: _torchEnabled
+                                    ? Colors.amberAccent
+                                    : Colors.white,
+                                onPressed: () async {
+                                  await _controller.toggleTorch();
+                                  setState(() {
+                                    _torchEnabled = !_torchEnabled;
+                                  });
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(
+                                  Icons.cameraswitch_outlined,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () async =>
+                                    await _controller.switchCamera(),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: CornerBorder(
+                    size: Size(200, 200),
+                    color: Theme.of(context).colorScheme.primary,
+                    borderRadius: 20,
+                    cornerLength: 40,
+                    strokeWidth: 5,
+                  ),
+                ),
+              ]),
+              errorBuilder: (context, error) {
+                return DefaultPageBody(
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 25.0),
+                      Icon(
+                        Icons.camera_alt_outlined,
+                        size: 64,
+                        color: Colors.redAccent,
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        'Unable to access the camera',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        error.toString(),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              onDetect: _handleScan,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
