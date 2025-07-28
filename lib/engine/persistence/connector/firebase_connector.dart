518529fde326fbb73df8d35edbb6a69044f09a95
grievous110 <130253759+grievous110@users.noreply.github.com>
2025-07-28 22:58:35 +0200

    This needs a reference now to the service
    - Now avoiding singletons

--- lib/engine/persistence/connector/firebase_connector.dart
+++ lib/engine/persistence/connector/firebase_connector.dart
@@ -4,39 +4,43 @@ import 'package:passwordmanager/engine/persistence/connector/persistence_connect
 class FirebaseConnector implements PersistenceConnector {
   String _cloudDocId;
   final String _cloudDocName;
+  final Firestore _firestoreServiceRef;
 
-  FirebaseConnector({required String cloudDocId, required String cloudDocName}) : _cloudDocName = cloudDocName, _cloudDocId = cloudDocId;
+  FirebaseConnector({required String cloudDocId, required String cloudDocName, required Firestore firestoreServiceRef})
+      : _cloudDocName = cloudDocName,
+        _cloudDocId = cloudDocId,
+        _firestoreServiceRef = firestoreServiceRef;
 
   @override
   String get name => _cloudDocName;
 
   /// Returns true if the connector is ready to be used (e.g., file exists, user is logged in)
   @override
-  Future<bool> get isAvailable async => Firestore.instance.auth.isUserLoggedIn;
+  Future<bool> get isAvailable => Future.value(_firestoreServiceRef.auth.isUserLoggedIn);
 
   /// Loads the formatted, encrypted data string from the source
   @override
   Future<String> load() async {
-    final Map<String, dynamic> doc = await Firestore.instance.getDocument('${Firestore.instance.userVaultPath}/$_cloudDocId', fieldMask: ['data']);
+    final Map<String, dynamic> doc = await _firestoreServiceRef.getDocument('${_firestoreServiceRef.userVaultPath}/$_cloudDocId', fieldMask: ['data']);
     return doc['fields']['data']['stringValue'];
   }
 
   /// Saves a new version of the formatted, encrypted data string
   @override
   Future<void> save(String formattedData) async {
-    await Firestore.instance.updateDocument('${Firestore.instance.userVaultPath}/$_cloudDocId', {'data': formattedData});
+    await _firestoreServiceRef.updateDocument('${_firestoreServiceRef.userVaultPath}/$_cloudDocId', {'data': formattedData});
   }
 
   /// Creates a new document or file with the given content (may be the same as save)
   @override
   Future<void> create(String formattedData) async {
-    String totalDocPath = await Firestore.instance.createDocument(Firestore.instance.userVaultPath, {'data': formattedData});
+    String totalDocPath = await _firestoreServiceRef.createDocument(_firestoreServiceRef.userVaultPath, {'data': formattedData});
     _cloudDocId = totalDocPath.split('/').last;
   }
 
   /// Optionally invalidates the session or state (e.g., logout, cache clear)
   @override
   Future<void> delete() async {
-    await Firestore.instance.deleteDocument('${Firestore.instance.userVaultPath}/$_cloudDocId');
+    await _firestoreServiceRef.deleteDocument('${_firestoreServiceRef.userVaultPath}/$_cloudDocId');
   }
 }
