e1c31db989b90182e35d6732a2f54945ac7aa311
Joel <grievous@posteo.de>
2026-03-07 23:45:11 +0100

    Correct linux header title and icon in task bar

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,12 +40,13 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "passwordmanager");
+    gtk_header_bar_set_title(header_bar, "Ethercrypt");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "passwordmanager");
+    gtk_window_set_title(window, "Ethercrypt");
   }
+  gtk_window_set_icon_from_file(window, "assets/appIcon.png", nullptr);
 
   gtk_window_set_default_size(window, 1280, 720);
   gtk_widget_show(GTK_WIDGET(window));
